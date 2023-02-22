134998d5816c06585e012ad93ddda37450e0e7df
seonggon yoo <gon85@naver.com>
2023-02-22 17:27:40 +0900

    테마를 적용하다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:principle_fe/app/controllers/global/auth_controller.dart';
 import 'package:principle_fe/app/data/providers/auth_api.dart';
 import 'package:principle_fe/app/data/repositories/auth/auth_repository.dart';
@@ -33,9 +34,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      // This theme was made for FlexColorScheme version 6.1.1. Make sure
+      // you use same or higher version, but still same major version. If
+      // you use a lower version, some properties may not be supported. In
+      // that case you can also remove them after copying the theme to your app.
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.bahamaBlue,
+        surfaceMode: FlexSurfaceMode.highSurfaceLowScaffold,
+        blendLevel: 9,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 10,
+          blendOnColors: false,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use the playground font, add GoogleFonts package and uncomment
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
       ),
+      darkTheme: FlexThemeData.dark(
+        scheme: FlexScheme.bahamaBlue,
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 15,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 20,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use the Playground font, add GoogleFonts package and uncomment
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+      ),
+      // If you do not have a themeMode switch, uncomment this line
+      // to let the device system mode control the theme mode:
+      // themeMode: ThemeMode.system,
+
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       initialRoute: AppRoutes.splash,
       getPages: AppPages.pages,
